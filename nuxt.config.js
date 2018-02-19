d1dfc05f7a7c7e912f834874f7eeccfb1d2f3b0a
Justin <justin.breg@ziggo.nl>
2018-02-19 21:43:48 +0100

    did something

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,46 @@
-var tailwindcss = require('tailwindcss');
+var tailwindcss = require('tailwindcss')
 
 module.exports = {
-  /*
+	/*
   ** Headers of the page
   */
-  head: {
-    title: 'osr_nuxt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
+	head: {
+		title: 'osr_nuxt',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'Nuxt.js project' }
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
+	/*
   ** Customize the progress bar color
   */
 	loading: { color: '#3B8070' },
 
 	css: ['@/assets/sass/main.scss'],
-  /*
+	/*
   ** Build configuration
   */
-  build: {
-    /*
+	build: {
+		/*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+		extend(config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
 		},
 
-		postcss: [
-			require('tailwindcss')('tailwind-config.js')
-		],
+		postcss: [require('tailwindcss')('tailwind-config.js')],
 
 		vendor: ['axios', 'vue-instantsearch']
 	},
 
-	plugins: [
-		'~plugins/vue-instantsearch'
-	]
+	plugins: ['~plugins/vue-instantsearch']
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,31 @@
 <template>
-  <section class="container">
+  <section class="tile-container">
+
+		<div class="sidebar bg-white">
+
+		</div>
+
+		<div class="tile__wrapper px-4">
+			<ais-index app-id="CGF9DFXU44" api-key="3ec1344cc4c046572bcc890c59bd931a" index-name="dev_OSR_catalog">
+				<ais-search-box></ais-search-box>
+				<ais-results>
+					<template slot-scope="{ result }">
+						<section class="tile p-4 mb-4">
+							{{ result.header }} <br>
+							{{ result.meta.date }}
+						</section>
+					</template>
+				</ais-results>
+			</ais-index>
+		</div>
 
   </section>
 </template>
 
 <script>
-
-export default {
-}
+export default {}
 </script>
 
 <style>
+
 </style>
